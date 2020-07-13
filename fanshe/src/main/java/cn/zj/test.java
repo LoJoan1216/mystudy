859ede1a2efd96aa6391c00fb0866584cfb291ae
Joan <lojoan@163.com>
2020-07-13 20:23:56 +0800

    chongtu

--- fanshe/src/main/java/cn/zj/test.java
+++ fanshe/src/main/java/cn/zj/test.java
@@ -3,5 +3,13 @@ package cn.zj;
 public class test {
     public static void main(String[] args) {
         System.out.println("!!!");
+        System.out.println("!!!");
+        System.out.println("!!!");
+        System.out.println("!!!");
+        add();
+    }
+
+    private static void add() {
+
     }
 }
